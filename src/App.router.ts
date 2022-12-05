46ac3ebc1582f4bb5dddd5272e3bf72cac189a3e
Andrew <andrewgeorgemitchell@gmail.com>
2022-12-04 21:02:22 -0800

    Fix cycle dependency

--- src/App.router.ts
+++ src/App.router.ts
@@ -1,36 +1,6 @@
 import { createReactRouter, createRouteConfig } from "@tanstack/react-router";
 import { Day01, Day02, Day03 } from "~/pages";
 
-export type DayRoute = string;
-
-export const DaysRoutes: DayRoute[] = [
-  "/",
-  "/day-02",
-  "/day-03",
-  // "/day-04",
-  // "/day-05",
-  // "/day-06",
-  // "/day-07",
-  // "/day-08",
-  // "/day-09",
-  // "/day-10",
-  // "/day-11",
-  // "/day-12",
-  // "/day-13",
-  // "/day-14",
-  // "/day-15",
-  // "/day-16",
-  // "/day-17",
-  // "/day-18",
-  // "/day-19",
-  // "/day-20",
-  // "/day-21",
-  // "/day-22",
-  // "/day-23",
-  // "/day-24",
-  // "/day-25",
-];
-
 const rootRoute = createRouteConfig();
 
 const day01Route = rootRoute.createRoute({
